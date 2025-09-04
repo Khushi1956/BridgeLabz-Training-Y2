dbb9866ce20ff93d0d6c40aac8c82c217ebab8ae
khushi1956 <khushi.singh_cs.aiml24@gla.ac.in>
2025-09-04 12:19:45 +0530

    [Khushi] Add :  Added Level-2 StudentGrades Solution

---
+++ Array/Level-2/StudentGrades.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class StudentGrades {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int[][] marks = new int[n][3]; // Physics, Chemistry, Maths
+        double[] percentage = new double[n];
+        String[] grade = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter marks for student " + (i + 1));
+            System.out.print("Physics: ");
+            marks[i][0] = sc.nextInt();
+            System.out.print("Chemistry: ");
+            marks[i][1] = sc.nextInt();
+            System.out.print("Maths: ");
+            marks[i][2] = sc.nextInt();
+
+            if (marks[i][0] < 0 || marks[i][1] < 0 || marks[i][2] < 0) {
+                System.out.println("Invalid input! Enter again.");
+                i--; // retry
+                continue;
+            }
+
+            int total = marks[i][0] + marks[i][1] + marks[i][2];
+            percentage[i] = total / 3.0;
+
+            if (percentage[i] >= 90) {
+                grade[i] = "A";
+            } else if (percentage[i] >= 75) {
+                grade[i] = "B";
+            } else if (percentage[i] >= 50) {
+                grade[i] = "C";
+            } else {
+                grade[i] = "F";
+            }
+        }
+
+        System.out.println("\nPhysics\tChemistry\tMaths\tPercentage\tGrade");
+        for (int i = 0; i < n; i++) {
+            System.out.println(marks[i][0] + "\t" + marks[i][1] + "\t\t" + marks[i][2] + "\t" + percentage[i] + "\t\t" + grade[i]);
+        }
+    }
+}
