5d88840e9fcd2321d3f0660684071a78ff48a563
khushi1956 <khushi.singh_cs.aiml24@gla.ac.in>
2025-09-04 11:46:43 +0530

    [Khushi] Add :  Added Level-1 TwoDToOneD Solution

---
+++ Array/Level-1/tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+
+//     public static void main(String[] args) {
+     
+//         CustomMethod obj = new CustomMethod();
+//         obj.CustomMethod(); 
+
+        
+//         int result = CustomMethod.sum(20, 30, 50);
+//         System.out.println(result);
+//     }
+// }
